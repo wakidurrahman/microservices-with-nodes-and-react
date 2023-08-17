c97a396a1378da0f3dcae0bcca3a16ee9d6c9d5e
wakidurrahman <wakidur.rahman@bjitgroup.com>
2023-08-17 18:23:19 +0900

    feat: implement docker

---
+++ docs/docker/step-to-create-nodejs-with-docker.md
@@ -0,0 +1,8 @@
+# Steps to create nodejs web App with docker
+
+1. Create NodeJS web app
+2. Create a Dockerfile
+3. Build image from dockerfile
+4. Run image as container
+5. Connect to web app from a browser
+
---
+++ init-project-001/index.js
@@ -0,0 +1,12 @@
+// step 01: create NodeJS web app
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
